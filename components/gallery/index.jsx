607718be1fab88e8db646979e75e58baf6c637c4
Raihan Ali <trueraihan@gmail.com>
2023-11-06 17:51:52 +0600

    documented gallery files

--- components/gallery/index.jsx
+++ components/gallery/index.jsx
@@ -11,18 +11,35 @@ import GallerySortable from "@/components/gallery/GallerySortable";
 import RemoveImage from "@/components/gallery/RemoveImage";
 import UploadImages from "./UploadImages";
 
+/**
+ * Gallery component for displaying and managing a sortable image gallery.
+ * @param {Object} props - Component properties.
+ * @param {Array} props.galleryImages - Array of images for the gallery.
+ * @returns {JSX.Element} - Gallery component.
+ */
 export default function Gallery({ galleryImages }) {
+  // State for managing gallery images and selected image IDs
   const [photos, setPhotos] = useState(galleryImages);
   const [selectedIds, setSelectedIds] = useState([]);
 
+  // Update gallery images when the prop changes
   useEffect(() => {
     setPhotos(galleryImages);
   }, [galleryImages, setPhotos]);
 
+  /**
+   * Handles the drag start event.
+   * @param {Object} event - Drag start event.
+   */
   const handleDragStart = (event) => {
+    // Uncomment the line below for debugging purposes
     // console.log("Drag Start:", event.active.id);
   };
 
+  /**
+   * Handles the drag end event, reordering the images in the gallery.
+   * @param {Object} event - Drag end event.
+   */
   const handleDragEnd = (event) => {
     const { active, over } = event;
     if (active.id === over.id) {
@@ -34,6 +51,8 @@ export default function Gallery({ galleryImages }) {
       return arrayMove(photos, oldIndex, newIndex);
     });
   };
+
+  // Render the Gallery component
   return (
     <DndContext
       collisionDetection={closestCenter}
@@ -41,13 +60,16 @@ export default function Gallery({ galleryImages }) {
       onDragEnd={handleDragEnd}
     >
       <SortableContext items={photos} strategy={rectSortingStrategy}>
+        {/* RemoveImage component for deleting selected images */}
         <RemoveImage
           selectedIds={selectedIds}
           photos={photos}
           setPhotos={setPhotos}
           setSelectedIds={setSelectedIds}
         />
+        {/* Gallery grid */}
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+          {/* Render each GallerySortable component for images */}
           {photos.map((photo, index) => (
             <GallerySortable
               photo={photo}
@@ -57,6 +79,7 @@ export default function Gallery({ galleryImages }) {
               setSelectedIds={setSelectedIds}
             />
           ))}
+          {/* UploadImages component for adding new images */}
           <UploadImages photos={photos} setPhotos={setPhotos} />
         </div>
       </SortableContext>
