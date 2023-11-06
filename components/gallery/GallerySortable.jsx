5550c82437609c91e79ca27d537dd4635b415de8
Raihan Ali <trueraihan@gmail.com>
2023-11-06 10:52:28 +0600

    hover effect added

--- components/gallery/GallerySortable.jsx
+++ components/gallery/GallerySortable.jsx
@@ -1,26 +1,27 @@
-"use client";
-
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import Image from "next/image";
 
 export default function GallerySortable({ photo, index }) {
-  console.log("photo: ", photo);
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({
-      id: photo.id,
-      transition: {
-        duration: 500,
-        easing: "cubic-bezier(0.25, 1, 0.5, 1)",
-      },
-    });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: photo.id,
+    transition: {
+      duration: 500,
+      easing: "cubic-bezier(0.25, 1, 0.5, 1)",
+    },
+  });
+
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
     backgroundImage: `url(${photo.photo})`,
-    height: `${index === 0 ? "539px" : "250px"}`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
+    height: `${index === 0 ? "510px" : "250px"}`,
   };
 
   return (
@@ -29,8 +30,12 @@ export default function GallerySortable({ photo, index }) {
       style={style}
       {...attributes}
       {...listeners}
-      className={` border border-gray-300  rounded-md  ${
+      className={`border border-gray-300 rounded-md bg-cover bg-center relative  hover:before:bg-gray-500 hover:before:absolute hover:before:w-full hover:before:h-full hover:before:opacity-50  ${
         index === 0 ? "col-span-2 row-span-2" : ""
+      } ${
+        isDragging
+          ? "shadow-lg scale-105"
+          : "hover:shadow-md transform transition-transform duration-300 ease-in-out"
       }`}
     ></div>
   );
