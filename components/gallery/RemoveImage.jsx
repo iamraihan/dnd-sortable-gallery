dd11994b47dbd9e387d223b7960e8b87a110f6e7
Raihan Ali <trueraihan@gmail.com>
2023-11-06 17:57:18 +0600

    document and optimize removeItems

--- components/gallery/RemoveImage.jsx
+++ components/gallery/RemoveImage.jsx
@@ -13,19 +13,36 @@ import {
 import { Button } from "@/components/ui/button";
 import useRemoveCards from "@/hooks/useRemoveCards";
 
+/**
+ * RemoveImage component for handling the removal of selected images.
+ * @param {Object} props - Component properties.
+ * @param {Array} props.selectedIds - Array of selected photo IDs.
+ * @param {Array} props.photos - Array of photos.
+ * @param {Function} props.setPhotos - Function to update the photos state.
+ * @param {Function} props.setSelectedIds - Function to update selected IDs state.
+ * @returns {JSX.Element} - RemoveImage component.
+ */
 export default function RemoveImage({
   selectedIds,
   photos,
   setPhotos,
   setSelectedIds,
 }) {
+  // Custom hook to handle image removal
   const updatedPhotos = useRemoveCards(photos, selectedIds);
+
+  /**
+   * Handles the deletion of selected images.
+   */
   const handleImageDeletion = () => {
     if (!selectedIds.length) return;
+
+    // Update photos and reset selected IDs
     setPhotos(updatedPhotos);
     setSelectedIds([]);
   };
 
+  // Render the RemoveImage component
   return (
     <div className="flex justify-between py-5">
       <p>Selected Items: {selectedIds.length} </p>
@@ -37,20 +54,19 @@ export default function RemoveImage({
           <AlertDialogContent>
             <AlertDialogHeader>
               <AlertDialogTitle>
-                {" "}
                 {selectedIds.length
                   ? "Are you absolutely sure? "
-                  : "Please select file"}
+                  : "Please select a file"}
               </AlertDialogTitle>
               <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
+                This action cannot be undone. This will permanently delete the
+                selected images.
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
               <AlertDialogCancel>Cancel</AlertDialogCancel>
               <AlertDialogAction onClick={handleImageDeletion}>
-                Delete{" "}
+                Delete
               </AlertDialogAction>
             </AlertDialogFooter>
           </AlertDialogContent>
